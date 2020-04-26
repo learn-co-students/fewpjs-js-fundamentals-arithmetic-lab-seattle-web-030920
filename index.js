64cea32b4c88caa9c5e6a7c2cb8eb929b7d342de
Bakugou808 <janu.m.sung@gmail.com>
2020-04-26 14:09:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+const divided = 10
+const divider = 6
+
+const nums = (1, 4, 5, 20)
+
+let multiply = num1*num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = divided % divider 
+
+let max = Math.max(nums)
